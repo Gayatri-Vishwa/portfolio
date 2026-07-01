23287d2165597ec26ee99779e266ef75d71b0c7c
Gayatri <142073085+Gayatri-Vishwa@users.noreply.github.com>
2026-07-01 22:56:36 +0530

    Update resume link in About component

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -47,7 +47,8 @@ function About() {
 
           {/* Resume Button */}
           <a 
-            href="https://drive.google.com/file/d/1rkCOf9hP_W3zI-DyjeNU27Bqrs5qNbwE/view?usp=drivesdk"
+            href="https://drive.google.com/file/d/17LZHJ489x4-AW91ezq_icDgdtflaUZwZ/view?usp=drivesdk"
+           // href="https://drive.google.com/file/d/1rkCOf9hP_W3zI-DyjeNU27Bqrs5qNbwE/view?usp=drivesdk"
                 //href="https://drive.google.com/file/d/1Mj18sJI8l7R84XvnJmcgJvCCfvS5fl-2/view?usp=drivesdk"
           
             target="_blank"
@@ -55,7 +56,7 @@ function About() {
   aria-label="Download Resume"
 
             rel="noopener noreferrer"
-            className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
+            className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg f"ont-bold transition duration-300 transform hover:scale-105"
             style={{
           
               background: "linear-gradient(90deg, #3b1d8f, #7b2ff7)",
